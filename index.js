21a33cfed295f75e1380521f169cb968f9ca2afa
babu97 <benkaimugul@gmail.com>
2023-09-23 06:24:19 +0300

    first class fuctions

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback){
+  callback()
+
+}
+function namedFunction() {
+  return "This is a named function";
+}
+
+// Define an anonymous function
+const anonymousFunction = function() {
+  return "This is an anonymous function";
+};
